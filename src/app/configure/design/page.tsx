3c37c40cdfc3b3f58202c49cf47e4cce26fa1ff7
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-06-04 16:50:40 +0500

    Design Page Started

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { notFound } from "next/navigation";
+import { db } from "@/db";
+import DesignConfigurator from "./DesignConfigurator";
+interface pageProps {
+  searchParams: {
+    [key: string]: string | string | undefined;
+  };
+}
 
-const page = () => {
-  return <div>page</div>;
+const Page = async ({ searchParams }: pageProps) => {
+  const { id } = searchParams;
+  //  IF NOT ID
+  if (!id || typeof id !== "string") return notFound();
+
+  //  AS THIS IS SERVER SIDE PAGE SO
+  const data = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  //  IF NO DATA FOUND
+  if (!data) return notFound();
+
+  //   AS WIDTH HEIGHT IMAGE URL IS STORED IN DATABASE
+  const { width, height, imageUrl } = data;
+
+  return (
+    <DesignConfigurator
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
+      configId={id}
+    />
+  );
 };
 
-export default page;
+export default Page;
