31f94355cea1065c5bed8847a7b435d46a287730
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-06-05 11:55:34 +0500

    Design Configurator Added

---
+++ src/components/HandleComponent.tsx
@@ -0,0 +1,8 @@
+//  TO show ball on drag and drop
+const HandleComponent = () => {
+  return (
+    <div className="w-5 h-5 rounded-full shadow border bg-white border-zinc-200 transition hover:bg-primary" />
+  );
+};
+
+export default HandleComponent;
