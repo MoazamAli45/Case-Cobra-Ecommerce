aea8b71a093dc9ab07e80eadc752531be7241b14
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-06-12 10:06:35 +0500

    Stripe Added

--- src/app/configure/preview/action.ts
+++ src/app/configure/preview/action.ts
@@ -1,6 +1,8 @@
+import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
 import { db } from "@/db";
+import { stripe } from "@/lib/stripe";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { configuration } from "@prisma/client";
+import { Order } from "@prisma/client";
 ("use server");
 
 export const createCheckoutSession = async ({
@@ -17,4 +19,60 @@ export const createCheckoutSession = async ({
   const { getUser } = getKindeServerSession();
   const user = await getUser();
   if (!user) throw new Error("User not found");
+
+  const { finish, material } = configuration;
+  let price = BASE_PRICE;
+  if (finish === "textured") price += PRODUCT_PRICES.finish.textured;
+  if (material === "polycarbonate")
+    price += PRODUCT_PRICES.material.polycarbonate;
+
+  let order: Order | undefined = undefined;
+
+  const existingOrder = await db.order.findFirst({
+    where: {
+      userId: user.id,
+      configurationId: configId,
+    },
+  });
+
+  if (existingOrder) {
+    order = existingOrder;
+  }
+
+  console.log("order", order);
+  console.log(user.id, configId, price, "USER  ID CONFIG ID PRICE");
+
+  if (!order) {
+    order = await db.order.create({
+      data: {
+        amount: price / 100,
+        userId: user.id,
+        configurationId: configuration.id,
+      },
+    });
+  }
+
+  const product = await stripe.products.create({
+    name: "Custom iPhone Case",
+    images: [configuration.imageUrl],
+    default_price_data: {
+      currency: "USD",
+      unit_amount: price,
+    },
+  });
+
+  const stripeSession = await stripe.checkout.sessions.create({
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
+    payment_method_types: ["card", "paypal"],
+    mode: "payment",
+    shipping_address_collection: { allowed_countries: ["PK", "US"] },
+    metadata: {
+      userId: user.id,
+      orderId: order.id,
+    },
+    line_items: [{ price: product.default_price as string, quantity: 1 }],
+  });
+
+  return { url: stripeSession.url };
 };
